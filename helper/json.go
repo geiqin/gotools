2110bb2706881c9ba659b267485d6cd0956c9c22
Tangyonghai <tech@geiqin.com>
2021-03-22 22:16:23 +0800

    Update json.go

--- helper/json.go
+++ helper/json.go
@@ -1,6 +1,11 @@
 package helper
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strconv"
+)
 
 func JsonDecode(jsonStr string, out interface{}) interface{} {
 	json.Unmarshal([]byte(jsonStr), &out)
@@ -10,4 +15,43 @@ func JsonDecode(jsonStr string, out interface{}) interface{} {
 func JsonEncode(obj interface{}) string {
 	str, _ := json.Marshal(obj)
 	return string(str)
+}
+
+func JsonToMSS(s string) map[string]string {
+	if s == "" {
+		return nil
+	}
+	msi := make(map[string]interface{})
+	err := json.Unmarshal([]byte(s), &msi)
+	if err != nil {
+		return nil
+	}
+	mss := make(map[string]string)
+	for k, v := range msi {
+		mss[k] = convertAnyToStr(v)
+	}
+	return mss
+}
+
+// 将任意类型转string
+func convertAnyToStr(v interface{}) string {
+	if v == nil {
+		return ""
+	}
+	switch d := v.(type) {
+	case string:
+		return d
+	case int, int8, int16, int32, int64:
+		return strconv.FormatInt(reflect.ValueOf(v).Int(), 10)
+	case uint, uint8, uint16, uint32, uint64:
+		return strconv.FormatUint(reflect.ValueOf(v).Uint(), 10)
+	case []byte:
+		return string(d)
+	case float32, float64:
+		return strconv.FormatFloat(reflect.ValueOf(v).Float(), 'f', -1, 64)
+	case bool:
+		return strconv.FormatBool(d)
+	default:
+		return fmt.Sprint(v)
+	}
 }
