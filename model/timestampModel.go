852da795ac4ef2f2122f1ff492dd259113037209
Tangyonghai <tech@geiqin.com>
2021-11-26 10:48:16 +0800

    Update timestampModel.go

--- model/timestampModel.go
+++ model/timestampModel.go
@@ -6,13 +6,22 @@ import (
 )
 
 type Timestamps struct {
+	CreatedAt LocalTime `json:"created_at" gorm:"type:datetime"`
+	UpdatedAt LocalTime `json:"updated_at" gorm:"type:datetime"`
+}
+
+type DelTimestamps struct {
+	DeletedAt gorm.DeletedAt `sql:"index" json:"deleted_at" gorm:"type:datetime"`
+}
+
+type OldTimestamps struct {
 	CreatedAt time.Time `json:"created_at" gorm:"type:datetime"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"type:datetime"`
 }
 
 type AllTimestamps struct {
-	CreatedAt time.Time `json:"created_at" gorm:"type:datetime"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"type:datetime"`
+	CreatedAt time.Time      `json:"created_at" gorm:"type:datetime"`
+	UpdatedAt time.Time      `json:"updated_at" gorm:"type:datetime"`
 	DeletedAt gorm.DeletedAt `sql:"index" json:"deleted_at" gorm:"type:datetime"`
 }
 
