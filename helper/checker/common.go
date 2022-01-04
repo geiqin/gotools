ef7a9c60576ab60cb49c0a0dbaa745f0188ca87d
Tangyonghai <tech@geiqin.com>
2022-01-04 22:36:10 +0800

    Update common.go

--- helper/checker/common.go
+++ helper/checker/common.go
@@ -2,6 +2,43 @@ package checker
 
 import "regexp"
 
+//数字+字母  不限制大小写 6~30位
+func IsID(str ...string) bool {
+	var b bool
+	for _, s := range str {
+		b, _ = regexp.MatchString("^[0-9a-zA-Z]{6,30}$", s)
+		if false == b {
+			return b
+		}
+	}
+	return b
+}
+
+//数字+字母+符号 6~30位
+func IsPwd(str ...string) bool {
+	var b bool
+	for _, s := range str {
+		b, _ = regexp.MatchString("^[0-9a-zA-Z@.]{6,30}$", s)
+		if false == b {
+			return b
+		}
+	}
+	return b
+}
+
+/************************* 数字类型 ************************/
+//纯整数
+func IsInteger(str ...string) bool {
+	var b bool
+	for _, s := range str {
+		b, _ = regexp.MatchString("^[0-9]+$", s)
+		if false == b {
+			return b
+		}
+	}
+	return b
+}
+
 //验证是否是手机
 func HasMobile(mobileNum string) bool {
 	//regular := "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$"
