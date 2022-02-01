63d340a2a85f718aeaa4104404d684538c4a14f1
Tangyonghai <tech@geiqin.com>
2022-02-01 17:23:54 +0800

    Update timestampModel.go

--- model/timestampModel.go
+++ model/timestampModel.go
@@ -25,48 +25,14 @@ type AllTimestamps struct {
 	DeletedAt gorm.DeletedAt `sql:"index" json:"deleted_at" gorm:"type:datetime"`
 }
 
-/*
-//MyTime 自定义时间
-type MyTime time.Time
-
-func (t *MyTime) UnmarshalJSON(data []byte) error {
-	if string(data) == "null" {
-		return nil
-	}
-	var err error
-	//前端接收的时间字符串
-	str := string(data)
-	//去除接收的str收尾多余的"
-	timeStr := strings.Trim(str, "\"")
-	t1, err := time.Parse("2006-01-02 15:04:05", timeStr)
-	*t = MyTime(t1)
-	return err
+//创建者
+type Creator struct {
+	CreatorId   int64  `json:"creator_id" gorm:"default:0"` //创建者ID
+	CreatorName string `json:"creator_name" gorm:"size:50"` //创建者名称
 }
 
-func (t MyTime) MarshalJSON() ([]byte, error) {
-	formatted := fmt.Sprintf("\"%v\"", time.Time(t).Format("2006-01-02 15:04:05"))
-	return []byte(formatted), nil
+//修改者
+type Modifier struct {
+	ModifierId   int64  `json:"modifier_id" gorm:"default:0"` //修改者ID
+	ModifierName string `json:"modifier_name" gorm:"size:50"` //修改者名称
 }
-
-func (t MyTime) Value() (driver.Value, error) {
-	// MyTime 转换成 time.Time 类型
-	tTime := time.Time(t)
-	return tTime.Format("2006-01-02 15:04:05"), nil
-}
-
-func (t *MyTime) Scan(v interface{}) error {
-	switch vt := v.(type) {
-	case time.Time:
-		// 字符串转成 time.Time 类型
-		*t = MyTime(vt)
-	default:
-		return errors.New("类型处理错误")
-	}
-	return nil
-}
-
-func (t *MyTime) String() string {
-	return fmt.Sprintf("hhh:%s", time.Time(*t).String())
-}
-
-*/
