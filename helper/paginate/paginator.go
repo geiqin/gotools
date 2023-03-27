de7537005f190d2cdba138410773ea68b4b36780
tangyonghai <tech@geiqin.com>
2023-03-27 23:22:28 +0800

    modify pager

--- helper/paginate/paginator.go
+++ helper/paginate/paginator.go
@@ -5,12 +5,12 @@ import (
 )
 
 type Paginator struct {
-	Paged     int
+	Paged     int64
 	Total     int64
-	PageCount int
-	PageSize  int
-	PrevPage  int
-	LastPage  int
+	PageCount int64
+	PageSize  int64
+	PrevPage  int64
+	LastPage  int64
 }
 
 type paginatorWrap struct {
@@ -25,16 +25,16 @@ type paginatorWrap struct {
 
 
 func New(paged int32, pageSize ...int32) *Paginator {
-	 p :=helper.Int32ToInt(paged)
+	 p,_ :=helper.ToInt64(paged)
 	 if pageSize !=nil {
-	 	s :=helper.Int32ToInt(pageSize[0])
+	 	s,_ :=helper.ToInt64(pageSize[0])
 	 	return NewFromInt(p,s)
 	 }
 	 return NewFromInt(p)
 }
 
-func NewFromInt(paged int, pageSize ...int) *Paginator {
-	var psize int
+func NewFromInt(paged int64, pageSize ...int64) *Paginator {
+	var psize int64
 	if pageSize != nil {
 		if pageSize[0] > 0 {
 			psize = pageSize[0]
@@ -57,15 +57,37 @@ func NewFromInt(paged int, pageSize ...int) *Paginator {
 
 
 func (a *Paginator) Offset() int {
-	offset := (a.Paged - 1) * a.PageSize
-	return offset
+	a.calculate()
+	var offset int64
+	p :=a.Paged - 1
+	if p>a.PageCount{
+		offset  = (a.PageCount - 1) * a.PageSize
+	}else{
+		offset  = (a.Paged - 1) * a.PageSize
+	}
+	return helper.StringToInt(helper.ToString(offset))
 }
 
 func (a *Paginator) Limit() int {
-	return a.PageSize
+	return helper.StringToInt(helper.ToString(a.PageSize))
+}
+
+func (a *Paginator) calculate()   {
+	a.PageCount = (a.Total +  a.PageSize - 1) / a.PageSize
+	a.LastPage = a.Paged + 1
+	a.PrevPage = a.Paged - 1
+	if a.LastPage > a.PageCount {
+		a.LastPage = a.PageCount
+	}
+	if a.PrevPage < 1 {
+		a.PrevPage = 1
+	}
 }
 
 func (a *Paginator) ToPager(pbPager interface{}) *interface{} {
+	a.calculate()
+	helper.ConvertData(pbPager, a)
+	/*
 	w:=&paginatorWrap{Paged: helper.IntToInt32(a.Paged)}
 	v :=helper.Int64ToString(a.Total)
 	t := helper.StringToInt32(v)
@@ -80,8 +102,8 @@ func (a *Paginator) ToPager(pbPager interface{}) *interface{} {
 	if a.PrevPage < 1 {
 		a.PrevPage = 1
 	}
-
 	helper.StructCopy(pbPager, w)
+	*/
 	return &pbPager
 }
 
