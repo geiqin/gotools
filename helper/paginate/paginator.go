1ac19684af6b36651786aea798b017a6ed2d5817
tangyonghai <tech@geiqin.com>
2023-03-28 09:20:07 +0800

    remove remark

--- helper/paginate/paginator.go
+++ helper/paginate/paginator.go
@@ -53,23 +53,6 @@ func (a *Paginator) calculate() {
 func (a *Paginator) ToPager(pbPager interface{}) *interface{} {
 	a.calculate()
 	helper.ConvertData(pbPager, a)
-	/*
-		w:=&paginatorWrap{Paged: helper.IntToInt32(a.Paged)}
-		v :=helper.Int64ToString(a.Total)
-		t := helper.StringToInt32(v)
-		w.Total =t
-		w.PageSize = helper.IntToInt32(a.PageSize)
-		w.PageCount = (t +  w.PageSize - 1) / w.PageSize
-		a.LastPage = a.Paged + 1
-		a.PrevPage = a.Paged - 1
-		if a.LastPage > a.PageCount {
-			a.LastPage = a.PageCount
-		}
-		if a.PrevPage < 1 {
-			a.PrevPage = 1
-		}
-		helper.StructCopy(pbPager, w)
-	*/
 	return &pbPager
 }
 
