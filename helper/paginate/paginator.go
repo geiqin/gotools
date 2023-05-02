d338bc3d50b92ba0b51bed28435fa871a300e709
tangyonghai <tech@geiqin.com>
2023-05-02 09:43:53 +0800

    modify pager

--- helper/paginate/paginator.go
+++ helper/paginate/paginator.go
@@ -13,12 +13,21 @@ type Paginator struct {
 	LastPage  int64 `json:"last_page"`
 }
 
-func New(paged int32, pageSize ...int32) *Paginator {
+func NewInt32(paged int32, pageSize ...int32) *Paginator {
 	entity := &Paginator{}
 	entity.Paged, _ = helper.ToInt64(paged)
 	if pageSize != nil {
 		s, _ := helper.ToInt64(pageSize[0])
 		entity.PageSize = s
+	}
+	return entity
+}
+
+func New(paged int64, pageSize ...int64) *Paginator {
+	entity := &Paginator{}
+	entity.Paged = paged
+	if pageSize != nil {
+		entity.PageSize = pageSize[0]
 	}
 	return entity
 }
