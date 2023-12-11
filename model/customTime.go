ad7ba8c96a0ee6e157ca223e06fc2e0e260d917d
tangyonghai <tech@geiqin.com>
2023-12-11 17:17:16 +0800

    d

--- model/customTime.go
+++ model/customTime.go
@@ -1,18 +1,30 @@
 package model
 
 import (
+	"database/sql/driver"
+	"github.com/geiqin/gotools/helper"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
-	"time"
 )
 
-type CustomTime struct {
-	time.Time
+type CustomTime string
+
+func (t CustomTime) GormDataType() string {
+	return "time"
+}
+func (t CustomTime) GormDBDataType(db *gorm.DB, field *schema.Field) string {
+	return "time"
 }
 
-func (CustomTime) GormDataType() string {
-	return "time"
+func (t CustomTime) Value() (driver.Value, error) {
+	if t == "" {
+		return nil, nil
+	}
+	return []byte(t), nil
 }
-func (CustomTime) GormDBDataType(db *gorm.DB, field *schema.Field) string {
-	return "time"
+
+func (t *CustomTime) Scan(v interface{}) error {
+	str := helper.ToString(v)
+	*t = CustomTime(str)
+	return nil
 }
