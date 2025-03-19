d432d1b1395a89506f343610e07e19238baf52b0
tangyonghai <tech@geiqin.com>
2025-03-20 00:45:52 +0800

    modify model

--- model/timestampModel.go
+++ model/timestampModel.go
@@ -2,25 +2,19 @@ package model
 
 import (
 	"gorm.io/gorm"
-	"time"
 )
 
 type Timestamps struct {
-	CreatedAt LocalTime `json:"created_at" gorm:"type:datetime;autoCreateTime;comment:创建时间"`
-	UpdatedAt LocalTime `json:"updated_at" gorm:"type:datetime;autoUpdateTime;comment:修改时间"`
+	UpdatedAt int64 `json:"updated_at" gorm:"autoUpdateTime:milli;comment:创建时间"` //创建时间
+	CreatedAt int64 `json:"created_at" gorm:"autoCreateTime:milli;comment:修改时间"` //修改时间
 }
 
 type DelTimestamps struct {
 	DeletedAt gorm.DeletedAt `sql:"index" json:"deleted_at" gorm:"type:datetime;comment:删除时间"`
 }
 
-type OldTimestamps struct {
-	CreatedAt time.Time `json:"created_at" gorm:"type:datetime"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"type:datetime"`
-}
-
 type AllTimestamps struct {
-	CreatedAt time.Time      `json:"created_at" gorm:"type:datetime;comment:创建时间"`
-	UpdatedAt time.Time      `json:"updated_at" gorm:"type:datetime;comment:修改时间"`
+	UpdatedAt int64          `json:"updated_at" gorm:"autoUpdateTime:milli;comment:创建时间"` //创建时间
+	CreatedAt int64          `json:"created_at" gorm:"autoCreateTime:milli;comment:修改时间"` //修改时间
 	DeletedAt gorm.DeletedAt `sql:"index" json:"deleted_at" gorm:"type:datetime;comment:删除时间"`
 }
